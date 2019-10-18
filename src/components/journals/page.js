54452069eb37112711927fccb5e88387a15bb0d3
rebeccanakamura <rebeccagithub@gmail.com>
2019-10-18 08:59:20 -0600

    deep dive props state project

---
+++ src/components/journals/page.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class $0 extends Component {
+
+        }
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
